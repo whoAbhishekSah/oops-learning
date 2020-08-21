b912c60e639d5cde18eba828761850c7f4da7911
Abhishek Sah <abhishek.sah@go-jek.com>
2020-08-21 19:22:22 +0530

    [Abhishek] Demo encapsulation - data hiding/privacy

--- demo2.cpp
+++ demo2.cpp
@@ -4,20 +4,75 @@ using namespace std;
 
 class Rectangle
 {
-    public:
+    private:
         int width;
         int length;
+
+    public:
         int area()
         {
             return width * length;
         }
 };
 
+// good design philiosphy for a class - Hide everything.
+
+class Address {
+    private:
+        string flat_number;
+        string customer_id;
+        string pin_code;
+        string landline;
+    public:
+        void setAddress();
+        void getPinCode(int customer_id);
+};
+
+//bank post wing takes work id and posts the atm card;
+class BankPostWing{
+    private:
+        int workId;
+        int customer_id;
+        int package_id;
+        Address addr;
+    public:
+        void sendPost();
+};
+
+//encapusaltion - Hiding the implementation
+class Bank{
+    private:
+        int[] customer_id;
+        int[] balances;
+        // string[] customer_name;
+        // char[][] customer_name;
+        // g
+        int x = journal[10]
+        bool is_authorized(){
+            return true;
+        }
+
+    public:
+        void withdraw(int customer_id, int amount){
+            //....
+        }
+        void deposit(int customer_id, int amount){
+            is_authorized()
+        }
+}
+
+
+/*benefits from encapsulation
+1. Fixes(decides) the interface
+2. security of data(attributes)
+3. Implementation can be changed, without changing the interface
+4.
+*/
+
 int main()
 {
     Rectangle r1;
     // r1.set(3, 4);
-
     int x = r1.area();
     r1.width = 3;
     r1.length = 4;
@@ -29,4 +84,6 @@ int main()
     r1.length = 5;
     cout<<r1.area()<<endl;
     return 0;
+
+    b1.deposit()
 }
