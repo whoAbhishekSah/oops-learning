e235a19fd3e8c5c18f3ae1d021988110cd3df7ea
Abhishek Sah <abhishek.sah@go-jek.com>
2020-08-29 15:51:49 +0530

    [Abhishek] name mangling demo for overloaded functions

--- demo14.cpp
+++ demo14.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 //add is overloaded
-//extended name -> 
+//extended name ->
 class MyClass{
     public:
-        float add(float x, float y){
+        float add(float x, float y){        //float _Z3addff(float x, float y)
             cout<<"add 1 called\n";
             return x + y;
         }
-        float add(int x, int y){
+        int add(int x, int y){                //int _Z3addii(int x, int y)
             cout<<"add 2 called\n";
             return x + y;
         }
